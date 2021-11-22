4ec5027e213fc03dcfc163f2539bcd223c884624
Max Novelli <max.novelli@ess.eu>
2021-11-22 13:58:14 +0100

    Updated status information provided by endpoint /

--- search-api/server/boot/root.js
+++ search-api/server/boot/root.js
@@ -5,9 +5,41 @@
 
 'use strict';
 
-module.exports = function(server) {
+module.exports = function (server) {
   // Install a `/` route that returns server status
   const router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get('/', (req, res) => {
+
+    const api_version = process.env.API_VERSION || "unknown";
+    const docker_image_version = process.env.DOCKER_IMAGE_VERSION || "unknown";
+    const hosting_facility = process.env.HOSTING_FACILITY || "unknown";
+    const environment = process.env.ENVIRONMENT || "unknown";
+
+
+    function format(seconds) {
+      function pad(s) {
+        return (s < 10 ? '0' : '') + s;
+      }
+      var hours = Math.floor(seconds / (60 * 60));
+      var minutes = Math.floor(seconds % (60 * 60) / 60);
+      var seconds = Math.floor(seconds % 60);
+
+      return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+    }
+
+    const uptime = process.uptime();
+
+    const response_string = {
+      'uptime_seconds': uptime,
+      'uptime': format(uptime),
+      'api_version': api_version,
+      'docker_image_version': docker_image_version,
+      'hosting_facility': hosting_facility,
+      'environment': environment
+    };
+
+    res.send(response_string);
+  });
+
   server.use(router);
 };
