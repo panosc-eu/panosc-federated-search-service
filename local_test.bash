b82984b52b1ece5bf9bddd763c45dc189bb9428a
Massimiliano Novelli <max.novelli@ess.eu>
2022-02-23 14:56:29 +0100

    updated local test script and CI test

--- local_test.bash
+++ local_test.bash
@@ -5,7 +5,13 @@
 
 clear
 
-export PROVIDERS="https://icatplus.esrf.fr/api,https://scicat.ess.eu/panosc-api,https://fairdata.ill.fr/fairdata/api"
+export API_VERSION="Current git commit"
+export DOCKER_IMAGE_VERSION="None"
+export HOSTING_FACILITY="Local Host"
+export ENVIRONMENT="Local instance with local data providers"
+export DEFAULT_LIMIT="10"
+#export PROVIDERS="https://icatplus.esrf.fr/api,https://scicat.ess.eu/panosc-api,https://fairdata.ill.fr/fairdata/api"
+export PROVIDERS="https://scicat.ess.eu/panosc-api,https://fairdata.ill.fr/fairdata/api"
 
 cd search-api
 
