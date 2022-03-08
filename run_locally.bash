e1b42867590772516aef2b684e07f05201e8e064
Fredrik Bolmsten <fredrik.bolmsten@ess.eu>
2022-03-08 14:37:04 +0100

    Make providers come from a single file

--- run_locally.bash
+++ run_locally.bash
@@ -10,12 +10,16 @@
 
 clear
 
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+
 export API_VERSION="Current git commit"
 export DOCKER_IMAGE_VERSION="None"
 export HOSTING_FACILITY="Local Host"
 export ENVIRONMENT="Local instance with official endpoints"
 export DEFAULT_LIMIT="10"
-export PROVIDERS="https://icatplus.esrf.fr/api,https://scicat.ess.eu/panosc-api,https://fairdata.ill.fr/fairdata/api,https://searchapi.maxiv.lu.se/api"
 
 cd search-api
 
